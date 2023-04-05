eafa8a4c6be5d7328e6e1ce71e5a4f744bac0db1
dondelion666 <91913477+dondelion666@users.noreply.github.com>
2023-04-05 15:43:40 +0100

    working!
    set pad_number to local

--- many_to_many.lua
+++ many_to_many.lua
@@ -1,4 +1,4 @@
-engine.name="many_to_many1"
+engine.name="many_to_many2"
 
 PADS=8
 BUFFERS=4
@@ -15,67 +15,38 @@ function init()
 for i=1,BUFFERS do
 
 params:add_file("buffer_file"..i,"buffer_file "..i,_path.audio)
-params:set_action("buffer_file"..i,function(x) engine.buffer_file(i,x) end)
+params:set_action("buffer_file"..i,function(x) print("buffer "..i.."file "..x) engine.buffer_file(i,x) end)
 
 end
 
 for i=0,BUFFERS-1 do
 for j=1,PADS do
   
-  pad_number=(i*PADS)+j
-  
+  local pad_number=(i*PADS)+j
+  print("pad number "..pad_number)
   toggles[pad_number]=0
 
-   params:add_group("loop_group"..pad_number,"loop "..pad_number,7)
+   params:add_group("loop_group"..pad_number,"loop "..pad_number,5)
   
   params:add_separator("loop_sep"..pad_number,"loop "..pad_number)
   params:add_number("loop_buffer"..pad_number,"loop_buffer "..pad_number,1,4,i+1)
-  params:set_action("loop_buffer"..pad_number,function(x) engine.loop_buffer(pad_number,x) end)
+  params:set_action("loop_buffer"..pad_number,function(x) print("loaded buffer "..x) engine.loop_buffer(pad_number,x) end)
   params:add_control("loop_start"..pad_number,"loop_start "..pad_number,controlspec.new(0,1,'lin',0.01,0,'%',0.01))
   params:set_action("loop_start"..pad_number,
     function(x) engine.loop_start(pad_number,x) engine.loop_end(pad_number,params:get("loop_length"..pad_number)+x) end)
   params:add_control("loop_length"..pad_number,"loop_length "..pad_number,controlspec.new(0,1,'lin',0.01,0.11,'%',0.01))
   params:set_action("loop_length"..pad_number,function(x) engine.loop_end(pad_number,params:get("loop_start"..pad_number)+x) end)
-  --[[params:add_binary("loop_looping"..i,"loop_looping "..i,"toggle",0)
-  params:set_action("loop_looping"..i,function(x) engine.loop_looping(i,x) end)]]--
-  
-  params:add_control("rate"..pad_number,"rate "..pad_number,controlspec.new(0,10,'lin',0.01,1,'x',0.01))
-  params:set_action("rate"..pad_number,function(x) engine.loop_rate(pad_number,x) end)
-  params:add_control("vol"..pad_number,"vol "..pad_number,controlspec.new(0,1,'lin',0.01,1,'',0.01))
-  params:set_action("vol"..pad_number,function(x) engine.loop_vol(pad_number,x) end)
+  params:add_control("loop_rate"..pad_number,"loop_rate "..pad_number,controlspec.new(0,10,'lin',0.01,1,'x',0.01))
+  params:set_action("loop_rate"..pad_number,function(x) engine.loop_rate(pad_number,x) end)
+  params:add_control("loop_vol"..pad_number,"loop_vol "..pad_number,controlspec.new(0,1,'lin',0.01,1,'',0.01))
+  params:set_action("loop-vol"..pad_number,function(x) engine.loop_vol(pad_number,x) end)
   
   params:add_binary("loop_play"..pad_number,"loop_play "..pad_number,"toggle",0)
-  params:set_action("loop_play"..pad_number,function(x) engine.loop_play(pad_number,params:get("loop_buffer"..pad_number),x) end)
+  params:set_action("loop_play"..pad_number,function(x)  engine.loop_play(pad_number,params:get("loop_buffer"..pad_number),x) end)
   
 end
 end
 
-end
-
-function key(n,d)
-  if n==2 then
-    alt=d
-  end
-end
-
-function enc(n,z)
-  if alt==0 then
-    if n==1 then
-      current_pad=util.clamp(current_pad+z,1,16)
-    elseif n==2 then
-      params:delta("loop_start"..current_pad,z)
-    elseif n==3 then
-      params:delta("loop_end"..current_pad,z)
-    end
-  elseif alt==1 then
-    if n==1 then
-    elseif n==2 then
-      params:delta("loop_rate"..current_pad,z)
-    elseif n==3 then
-      params:delta("loop_vol"..current_pad,z)
-    end
-  end
-  redraw()
 end
 
 function getfilename(path)
@@ -86,16 +57,17 @@ end
 g = grid.connect()
 
 g.key = function(x,y,z)
-  local pad_number=((y-1)*PADS)+x
+  local pad_pressed=((y-1)*PADS)+x
+  print("pad "..pad_pressed)
   current_pad=x
-  if x<=8 and toggles[pad_number]==0 and z==1 then
-    params:set("loop_play"..pad_number,1)
+  if x<=8 and toggles[pad_pressed]==0 and z==1 then
+    params:set("loop_play"..pad_pressed,1)
     g:led(x,y,15)
-    toggles[pad_number]=1
-  elseif x<=8 and toggles[pad_number]==1 and z==1 then
-    params:set("loop_play"..pad_number,0)
+    toggles[pad_pressed]=1
+  elseif x<=8 and toggles[pad_pressed]==1 and z==1 then
+    params:set("loop_play"..pad_pressed,0)
     g:led(x,y,0)
-    toggles[pad_number]=0
+    toggles[pad_pressed]=0
   end
   g:refresh()
   redraw()
